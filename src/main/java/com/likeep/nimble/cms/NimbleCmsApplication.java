cb9064d023b0250c571d6198fca8f2a2875da1b6
yanqiuhuang188 <yanqiuhuang188@163.com>
2018-06-03 17:53:00 +0800

    first commit

---
+++ src/main/java/com/likeep/nimble/cms/NimbleCmsApplication.java
@@ -0,0 +1,13 @@
+package com.likeep.nimble.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimbleCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimbleCmsApplication.class, args);
+		System.out.println();
+	}
+}
