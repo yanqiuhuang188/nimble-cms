98463916a901892844880bde0f623cc9dbed350f
yanqiuhuang188 <yanqiuhuang188@163.com>
2018-07-22 22:47:39 +0800

    update

---
+++ src/main/static/src/models/report.js
@@ -0,0 +1,47 @@
+import { queryReport, removeReport, addReport } from '../services/api';
+
+export default {
+  namespace: 'report',
+
+  state: {
+    data: {
+      list: [],
+      pagination: {},
+    },
+  },
+
+  effects: {
+    *fetch({ payload }, { call, put }) {
+      const response = yield call(queryReport, payload);
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+    },
+    *add({ payload, callback }, { call, put }) {
+      const response = yield call(removeReport, payload);
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+      if (callback) callback();
+    },
+    *remove({ payload, callback }, { call, put }) {
+      const response = yield call(addReport, payload);
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+      if (callback) callback();
+    },
+  },
+
+  reducers: {
+    save(state, action) {
+      return {
+        ...state,
+        data: action.payload,
+      };
+    },
+  },
+};
